8fd089819155ddeb858b8087ab63f1c35ccc0b19
BathalaSatwik <chowdarybsatwik@gmail.com>
2025-04-24 12:34:50 +0530

    Add files via upload

--- Alex.java
+++ Alex.java
@@ -1,5 +1,11 @@
-class Demo{
- public static void main(String args[]){
-  System.out.println("Welcome");
-}
+public class Alex{
+  public static void main(String [] args){
+    //performing basic opertaions in sopln
+    System.out.println(2+2);
+    System.out.println(3-1);
+    System.out.println(3*3);
+    System.out.println(35/5);
+    System.out.println(45%5);
+
+  }
 }
