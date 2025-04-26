fe8471b6413c20eefa6c4e186a82c9fa81ed45d1
BathalaSatwik <chowdarybsatwik@gmail.com>
2025-04-26 09:51:25 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,11 @@
+public class Ternary {
+    public static void main(String[] args){
+        int x =100;
+        String result="none"; // use of ternary operator
+        result= x>0?"positive":"Negative";
+        System.out.println(result);
+
+
+    }
+    
+}
