10cff2376b8807ad7f73cd26aaff5af96b07e970
BathalaSatwik <chowdarybsatwik@gmail.com>
2025-05-25 12:23:04 +0530

    Add files via upload

--- Static2.java
+++ Static2.java
@@ -1,38 +1,33 @@
-import java.util.Scanner;
-class Farmer{
-    float p;
-    float t;
-    float si;
-    float f;
-    static float r;
-
-static {
-    r=2.5f;
-}
-void acceptInput(){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the principal Amount:");
-    p=sc.nextFloat();
-    System.out.println("Enter the time duration:");
-    t=sc.nextFloat();
-}
-void compute(){
-    si=(p*t*r)/100;
-}
-void finalAmount(){
-    f=p+si;
-}
-void display(){
-    System.out.println("This is your Simple-interst Amount:"+si);
-    System.out.println("This is the final Amount:"+f);
-}
-}
-public class Static2{
-    public static void main(String[] args){
-        Farmer f=new Farmer();
-        f.acceptInput();
-        f.compute();
-        f.finalAmount();
-        f.display();
+package vivek;
+class Student1{
+    int roll; 
+    String name;
+    int marks;
+    static int yop=2025; //definig how to access the instance variables in static method.
+    
+    static void alex(Student1 stud) {
+    	System.out.println("Roll: "+stud.roll+"Name: "+stud.name+"marks: "+stud.marks+"yop: "+yop);
+    	
+    	
+    }
+    
+    void display(){
+        System.out.println("Roll: "+roll+"Name: "+name+"marks: "+marks+"yop: "+yop);
     }
 }
+
+public class Static2 {
+	    public static void main(String [] args){
+	        Student1 stud=new Student1();
+	        stud.roll=1;
+	        stud.name="Satwik";
+	        stud.marks=89;
+	        Studs stud1=new Studs();
+	        stud1.roll=2;
+	        stud1.name="vivek";
+	        stud1.marks=99;
+	        stud.display();
+	        stud1.display();
+	        Student1.alex(stud);
+}
+	}
